71ebaaada389b3a960107b6988169d7c39c3e856
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-11-04 01:09:30 +0100

    Update push_all.sh

--- push_all.sh
+++ push_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export repo='dennis@192.168.188.4:/var/luna/git/traefik/assets/arkanerepo-repo/'
+export repo='dennis@192.168.188.4:/var/luna/git/traefik/assets/arkanerepo-repo/arkane/os/x86_64/'
 
 scp ./arkane.* $repo
 scp ./*/*.pkg.tar.zst $repo
