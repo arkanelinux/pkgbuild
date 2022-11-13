241579d754fd2262e2b12cdadc838cb3f46fff4c
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-11-13 13:59:01 +0100

    Allow vars in export

--- push_all.sh
+++ push_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export repo='$USER@192.168.188.4:/var/luna/git/traefik/assets/arkanerepo-repo/arkane/os/x86_64/'
+export repo="$USER@192.168.188.4:/var/luna/git/traefik/assets/arkanerepo-repo/arkane/os/x86_64/"
 
 scp ./arkane.* $repo
 scp ./*/*.pkg.tar.zst $repo
