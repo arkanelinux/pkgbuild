1e65215d08c4a943bf67d4e6d167b8c1143533de
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-11-23 19:02:54 +0100

    Ensure pacman-init.service is finished running before starting install

--- os-installer-config/prepare.sh
+++ os-installer-config/prepare.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+# Infinite loop until pacman-init.service finishes, this is to prevent 
+# the user from racing through the os-installer and starting pacman
+# before it finishes resulting in pacman gpg key errors
+echo "Waiting for pacman-init.service to finish running before starting the installation..."
+while true; do
+	systemctl status pacman-init.service | grep "Finished Initializes Pacman keyring."
+
+	if [ $? -eq 0 ];
+	then
+		echo "pacman-init.service has finished, starting the installation process..."
+		break
+	fi
+
+	sleep 2
+done
+
 # Synchornize with repos
 sudo pacman -Syy
 
